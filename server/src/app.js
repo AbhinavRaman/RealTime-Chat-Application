b762debc8a87ded49a53f9368904167d64e227d7
AbhinavRaman <abhinavraman102@gmail.com>
2026-01-29 11:58:36 +0530

    User registration with password hashing

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 
+const authRoutes = require("./routes/auth.routes");
+
 const app = express();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 // health check route
 app.get("/", (req, res) => {
