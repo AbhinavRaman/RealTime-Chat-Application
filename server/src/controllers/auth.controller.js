8ab2e3477a06062153ca73ac02b9f1ec0838cac9
AbhinavRaman <abhinavraman102@gmail.com>
2026-02-03 15:58:49 +0530

    Login API with JWT authentication done

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+// Register User
 const registerUser = async (req, res) => {
     try{
         const {name, email, password} = req.body;
@@ -48,4 +50,59 @@ const registerUser = async (req, res) => {
     }
 };
 
-module.exports = { registerUser };
+// Login User
+const loginUser = async (req,res) => {
+    try{
+        const{ email, password } = req.body;
+
+        //Validation
+        if(!email || !password){
+            return res.status(400).json({
+                message: "Email and password are required",
+            });
+        }
+
+        //Check if the user exists
+        const user = await User.findOne({ email });
+        if(!user){
+            return res.status(401).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        //compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(401).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        // generate jwt
+        const token = jwt.sign(
+            { userId: user._id},
+            process.env.JWT_SECRET,
+            {
+                expiresIn: process.env.JWT_EXPIRES_IN,
+            }
+        );
+
+        // SEND response
+        res.status(200).json({
+            message: "Login Successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+            },
+        });
+    } catch (error) {
+        console.error("Login error:", error);
+        res.status(500).json({
+            message: "Server Error",
+        });
+    }
+};
+
+module.exports = { registerUser, loginUser };
